4c5eb1926762a29a070e69fa276caeb99ba577b8
eseulssi <eeeeeseulllll@naver.com>
2023-03-29 17:38:52 +0900

    test12

--- src/chap18_enum/_07_useOfSeason.java
+++ src/chap18_enum/_07_useOfSeason.java
@@ -1,5 +1,16 @@
 package chap18_enum;
 
+import chap18_enum.enums.Season;
+
 public class _07_useOfSeason {
+	public static void main(String[] args) {
+		
+		Season[] seasonArr = Season.values();
+		
+				
+				for(Season s : seasonArr) {
+					s.printseasonMonth();
+				}
+	}
 
 }
---
+++ src/hhhhhh/hihi.java
@@ -0,0 +1,10 @@
+package hhhhhh;
+
+public class hihi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//helloworld
+	}
+
+}
